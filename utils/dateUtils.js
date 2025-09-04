1c8b97369c8cc3a1ab56a2ac64aa4a759d198db6
Trương Gia <hailamttkc@gmail.com>
2025-09-04 15:27:38 +0700

    Update dateUtils.js
    Fix cuối

--- utils/dateUtils.js
+++ utils/dateUtils.js
@@ -3,15 +3,9 @@ export const formatDate = (date) => date.toISOString().split('T')[0];
 export const getWeekDays = (date) => {
     const startOfWeek = new Date(date);
     const day = startOfWeek.getDay();
-    // Điều chỉnh để tuần bắt đầu từ Thứ Hai (Monday)
-    const diff = startOfWeek.getDate() - day + (day === 0 ? -6 : 1);
+    const diff = startOfWeek.getDate() - day + (day === 0 ? -6 : 1); // Adjust when week starts on Monday
     startOfWeek.setDate(diff);
-    
-    // Tạo một mảng chứa 7 ngày trong tuần
-    return Array.from({ length: 7 }).map((_, i) => {
-        const newDate = new Date(startOfWeek);
-        newDate.setDate(startOfWeek.getDate() + i);
-        return newDate;
-    });
+    return Array.from({ length: 7 }).map((_, i) => new Date(startOfWeek.getFullYear(), startOfWeek.getMonth(), startOfWeek.getDate() + i));
 };
+
 
