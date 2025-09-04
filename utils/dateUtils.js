e9622a005d19bc66171deeeaf5a1327d90a42055
Trương Gia <hailamttkc@gmail.com>
2025-09-04 14:42:51 +0700

    Update dateUtils.js

--- utils/dateUtils.js
+++ utils/dateUtils.js
@@ -3,7 +3,15 @@ export const formatDate = (date) => date.toISOString().split('T')[0];
 export const getWeekDays = (date) => {
     const startOfWeek = new Date(date);
     const day = startOfWeek.getDay();
+    // Điều chỉnh để tuần bắt đầu từ Thứ Hai (Monday)
     const diff = startOfWeek.getDate() - day + (day === 0 ? -6 : 1);
     startOfWeek.setDate(diff);
-    return Array.from({ length: 7 }).map((_, i) => new Date(startOfWeek.getFullYear(), startOfWeek.getMonth(), startOfWeek.getDate() + i));
+    
+    // Tạo một mảng chứa 7 ngày trong tuần
+    return Array.from({ length: 7 }).map((_, i) => {
+        const newDate = new Date(startOfWeek);
+        newDate.setDate(startOfWeek.getDate() + i);
+        return newDate;
+    });
 };
+
